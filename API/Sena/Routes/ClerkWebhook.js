597eada29313f599a60cb532059a19cdd641f9c7
josy12 <josmer1997@hotmail.es>
2025-04-20 16:01:06 -0500

    avance clerk

--- API/Sena/Routes/ClerkWebhook.js
+++ API/Sena/Routes/ClerkWebhook.js
@@ -1,24 +1,110 @@
-import { Router } from 'express'
+import e, { Router } from 'express'
+import { DBPostgres } from '../../BDPostgres'
+import { clerkClient } from '@clerk/express'
 
 const clerkwebhook = Router()
 
 clerkwebhook.post('/webhook', async (req, res) => {
   const evento = req.body
-
-  const { id, email_addresses, first_name, last_name } = evento.data
-  // /////////////
   console.log(evento.type)
-  console.log('ID:', id)
-  console.log('Email:', email_addresses[0].email_address)
-  console.log('Nombre:', first_name)
-  console.log('Apellido:', last_name)
-  // Aquí puedes manejar el evento según tus necesidades
+  const { id, email_addresses, first_name } = evento.data
 
-  try {
-    return res.status(200).json({ message: 'Evento procesado correctamente' })
-  } catch (error) {
-    return res.status(500).json({ error: 'error en webhook' })
+  if (evento) {
+    try {
+      const usuario = await DBPostgres.query(
+        'SELECT * FROM sena.usuarios WHERE id = $1',
+        [id]
+      )
+
+      switch (evento.type) {
+        case 'user.created':
+          if (usuario.rows.length) {
+            console.log({ error: 'Usuario ya existe' })
+            return res.status(400).json({ error: 'Usuario ya existe' })
+          }
+          const creado = await DBPostgres.query(
+            'INSERT INTO sena.usuarios (id, email, nombre) VALUES ($1, $2, $3) returning id',
+            [id, email_addresses[0].email_address, first_name]
+          )
+          if (creado.rows.length) {
+            // inserta el carrito dentro del usuario
+            await DBPostgres.query(
+              `insert into sena.Carritos (id) values($1)`,
+              [id]
+            )
+            // inserta los atributos del usuario
+            await DBPostgres.query(
+              `INSERT INTO sena.AtributosUsuarios (id,administrador,baneado) values($1,FALSE,FALSE)
+ `,
+              [id]
+            )
+            await clerkClient.users.updateUserMetadata(id, {
+              publicMetadata: {
+                administrador: false,
+                baneado: false
+              }
+            })
+          }
+
+          if (!creado.rows.length) {
+            return res.status(500).json({ error: 'error al crear el usuario' })
+          }
+          console.log('Usuario creado:', id)
+          return res
+            .status(200)
+            .json({ message: 'Usuario creado correctamente' })
+        // finaliza la creacion del usuario
+        case 'user.updated':
+          if (!usuario.rows.length) {
+            console.log({ error: 'Usuario no existe' })
+            return res.status(400).json({ error: 'Usuario no existe' })
+          }
+          // await DBPostgres.query(
+          //   'UPDATE sena.usuarios SET email = $1, nombre = $2  WHERE id = $3',
+          //   [email_addresses[0].email_address, first_name, id]
+          // )
+          console.log('Usuario actualizado:', evento.data)
+
+          console.log('Usuario actualizado:', actualizado)
+          return res
+            .status(200)
+            .json({ message: 'Usuario actualizado correctamente' })
+        // finaliza la actualizacion del usuario
+        case 'user.deleted':
+          if (!usuario.rows.length) {
+            console.log({ error: 'Usuario no existe' })
+            // Eliminar el usuario de la base de datos
+
+            return res.status(400).json({ error: 'Usuario no existe' })
+          }
+          // Eliminar el usuario de la base de datos
+          // Puedes usar el ID del evento para identificar al usuario
+          await DBPostgres.query('DELETE FROM sena.usuarios WHERE id = $1 ', [
+            id
+          ])
+          await DBPostgres.query(`delete from sena.Carritos where id = $1`, [
+            id
+          ])
+          await DBPostgres.query(
+            `delete from sena.AtributosUsuarios where id = $1
+ `,
+            [id]
+          )
+          console.log('Usuario eliminado', id)
+          return res
+            .status(200)
+            .json({ message: 'Usuario eliminado correctamente' })
+        // finaliza la eliminacion del usuario
+        default:
+          return res.status(400).json({ error: 'Evento no manejado' })
+      }
+    } catch (error) {
+      return res.status(500).json({ error: 'error en webhook' })
+    }
   }
+  return res
+    .status(400)
+    .json({ error: 'Evento no manejado error al recibir peticion' })
 })
 
 export default clerkwebhook
